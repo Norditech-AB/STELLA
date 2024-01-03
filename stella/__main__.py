8ec47e927e659dd0864f16830af50f305bf55693
David Bacelj <david@Davids-MacBook-Air.local>
2024-01-03 09:43:06 +0100

    fixed pipe bug

--- stella/__main__.py
+++ stella/__main__.py
@@ -1,23 +1,29 @@
 import os
 import sys
 
-def add_current_directory_to_pythonpath():
-    # Get the directory where the script is run from
-    current_directory = os.getcwd()
+def init():
 
-    # Get the current PYTHONPATH
-    current_pythonpath = os.environ.get('PYTHONPATH', '')
+    def add_current_directory_to_pythonpath():
+        # Get the directory where the script is run from
+        current_directory = os.getcwd()
 
-    # Construct the new PYTHONPATH
-    new_pythonpath = f"{current_pythonpath}{os.pathsep}{current_directory}" if current_pythonpath else current_directory
+        # Get the current PYTHONPATH
+        current_pythonpath = os.environ.get('PYTHONPATH', '')
 
-    # Set the new pythonpaths
-    os.environ['PYTHONPATH'] = new_pythonpath
-    sys.path.append(new_pythonpath)
+        # Construct the new PYTHONPATH
+        new_pythonpath = f"{current_pythonpath}{os.pathsep}{current_directory}" if current_pythonpath else current_directory
+
+        # Set the new pythonpaths
+        os.environ['PYTHONPATH'] = new_pythonpath
+        sys.path.append(new_pythonpath)
 
 
-add_current_directory_to_pythonpath()
+    add_current_directory_to_pythonpath()
 
-from stella.main import main
+    from stella.main import main
 
-main()
+    main()
+
+
+if __name__ == '__main__':
+    init()
