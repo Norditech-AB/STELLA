4aaec471da8cbedf0a2c782aa52c6ab122fe039c
David Bacelj <david@Davids-MacBook-Air.local>
2024-01-04 11:29:46 +0100

    fixed paths for both pip and working dir

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,29 +1,46 @@
+import json
 import os
-import sys
+import argparse
 
-def init():
+from cli.client.stella_client import StellaClient
+from cli.shell import Shell
 
-    def add_current_directory_to_pythonpath():
-        # Get the directory where the script is run from
-        current_directory = os.getcwd()
-
-        # Get the current PYTHONPATH
-        current_pythonpath = os.environ.get('PYTHONPATH', '')
-
-        # Construct the new PYTHONPATH
-        new_pythonpath = f"{current_pythonpath}{os.pathsep}{current_directory}" if current_pythonpath else current_directory
-
-        # Set the new pythonpaths
-        os.environ['PYTHONPATH'] = new_pythonpath
-        sys.path.append(new_pythonpath)
+from cli.commands import ServeCommand, ConfigureCommand
 
 
-    add_current_directory_to_pythonpath()
+def setup_parser():
+    parser = argparse.ArgumentParser(description="Stella Command Line Interface")
+    subparsers = parser.add_subparsers(dest='command', help='Available commands')
 
-    from cli.main import main
+    ServeCommand.init_parser(subparsers)
+    ConfigureCommand.init_parser(subparsers)
 
-    main()
+    return parser
+
+
+def main():
+    # Load config
+    with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
+        config = json.load(f)
+
+    # Create client
+    client = StellaClient(
+        host=config['host'],
+        port=config['port'],
+    )
+
+    # Setup parser and parse arguments (if any)
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    # If there's a command, execute it, otherwise start the shell
+    if args.command:
+        command = args.command_class(client, args)
+        command.execute()
+    else:
+        shell = Shell(client)
+        shell.start()
 
 
 if __name__ == '__main__':
-    init()
+    main()
