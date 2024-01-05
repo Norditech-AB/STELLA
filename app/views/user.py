54adb3c6f7a3a332f7d63919d3081bad5ab20c28
Philip Alm <philip@norditech.se>
2024-01-05 08:27:50 +0100

    Added endpoints for changing user username and password

--- app/views/user.py
+++ app/views/user.py
@@ -1,3 +1,6 @@
+import os
+
+import bcrypt
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
@@ -8,12 +11,14 @@ dotenv.load_dotenv()
 
 user_views = Blueprint('user_views', __name__)
 
+BCRYPT_SALT = os.getenv('BCRYPT_SALT')
 
-@user_views.route('/user', methods=['PUT'])
+
+@user_views.route('/user/username', methods=['PUT'])
 @jwt_required()
-def update_user():
+def change_username():
     """
-    Update the settings for a user
+    Change the username for a user
     :return:
     """
     json_data = request.get_json()
@@ -27,10 +32,47 @@ def update_user():
 
     # Get the settings from the request (if they exist)
     username = json_data.get('username', None)
-    if username is not None:
-        user.username = username
 
-    return jsonify({"msg": "User updated"}), 200
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+
+    user.username = username
+
+    db.update_user(user)
+
+    return jsonify({"msg": "Username updated"}), 200
+
+
+@user_views.route('/user/password', methods=['PUT'])
+@jwt_required()
+def change_password():
+    """
+    Change the password for a user
+    :return:
+    """
+    json_data = request.get_json()
+    user_id = get_jwt_identity()
+
+    # Find the user in the database
+    try:
+        user = db.get_user_by_id(user_id)
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 404
+
+    # Get the settings from the request (if they exist)
+    password = json_data.get('password', None)
+
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    pwd = bcrypt.hashpw(password.encode('utf-8'), BCRYPT_SALT.encode('utf-8'))
+
+    if password is not None:
+        user.password = pwd
+
+    db.update_user(user)
+
+    return jsonify({"msg": "Password updated"}), 200
 
 
 @user_views.route('/user', methods=['GET'])
